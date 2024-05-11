31213b77e699351b2a3cfda691668afc80eb9633
Potato-Mash-Coding <freddycw03@gmail.com>
2024-05-11 00:31:13 -0400

    S.A.3 hosting on heroku

--- src/commands/tools/rps.js
+++
@@ -1,8 +0,0 @@
-const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
-
-module.exports = {
-    data: new SlashCommandBuilder()
-        .setName('拋骰子')
-        .setDescription('骰子')
-    
-}
